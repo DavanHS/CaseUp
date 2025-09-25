16e79c485b651b27b6ed65b208356bff76261bc4
Davan <davanhs470@gmail.com>
2025-09-25 22:33:08 +0530

    feat: Add Design Configurator with customization options
    - Implemented DesignConfigurator component for customizing phone cases.
    - Integrated image rendering and configuration saving functionality.
    - Added utility function for formatting prices.
    - Introduced new UI components: AspectRatio, ScrollArea, DropdownMenu, and Label.
    - Updated product pricing configuration and option validators for materials and finishes.
    - Enhanced package dependencies with Radix UI and Headless UI components.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['b1rhbrw3mq.ufs.sh']
+  }
 };
 
 export default nextConfig;
